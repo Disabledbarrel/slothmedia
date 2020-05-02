d56f58cfc0c79ec37da296ad1efbf57d85f4c29b
Disabledbarrel <elin.elisabeth.larsson@gmail.com>
2020-05-02 16:33:10 +0200

    added Redux to the project. Setup redux-store

--- slothapp/client/src/App.js
+++ slothapp/client/src/App.js
@@ -4,21 +4,26 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
+
 import './App.css';
 
 const App = () => 
-<Router>
-    <Fragment>
-        <Navbar />
-        <Route exact path ='/' component={Landing} />
+<Provider store={store}>
+    <Router>
         <Fragment>
-            <Switch>
-                <Route exact path="/register" component={Register} />
-                <Route exact path="/login" component={Login} />
-            </Switch>
+            <Navbar />
+            <Route exact path ='/' component={Landing} />
+            <Fragment>
+                <Switch>
+                    <Route exact path="/register" component={Register} />
+                    <Route exact path="/login" component={Login} />
+                </Switch>
+            </Fragment>
         </Fragment>
-    </Fragment>
-</Router>
-    
+    </Router>
+</Provider>
 
 export default App;
